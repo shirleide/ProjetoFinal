7fc35f9910766b28e11a05f07030d4c7ce654f25
shirleide <shirleidevips@gmail.com>
2020-03-25 20:48:45 -0300

    Criação de componentes
    Criação dos componentes, produto, cliente e venda

--- sistemaVendas/src/app/app.module.ts
+++ sistemaVendas/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClienteComponent } from './cliente/cliente.component';
+import { ProdutoComponent } from './produto/produto.component';
+import { VendaComponent } from './venda/venda.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClienteComponent,
+    ProdutoComponent,
+    VendaComponent
   ],
   imports: [
     BrowserModule,
